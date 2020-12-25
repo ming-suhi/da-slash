88023bbd7277c649b9a9a8ed8ff1e6acbbf18bbd
xlyr.on <k.j.b.taton@gmail.com>
2020-12-25 20:30:44 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-const config = require('./config.json');
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const Slash = require('./da-slash.js');
-const slash = new Slash.Client(client, config);
-
-client.once('ready', () => {
-  slash.postCommands();
-})
-
-client.ws.on('INTERACTION_CREATE', async interaction => {
-  slash.matchCommand(interaction);
-})
-
-client.login(config.bot.token)
-
-
+const Client = require('./client.js');
+const Command = require('./command.js');
+const Interaction = require('./interaction.js');
+exports.Client = Client;
+exports.Command = Command;
+exports.Interaction = Interaction;
